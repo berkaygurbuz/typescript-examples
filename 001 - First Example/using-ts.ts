16ff291bcad983fc69ea50d05000525b47256a4e
Berkay Gürbüz <berkay-gurbuz@hotmail.com>
2021-02-02 02:54:15 +0300

    Initial commit

---
+++ 001 - First Example/using-ts.ts
@@ -0,0 +1,11 @@
+const button = document.querySelector("button");
+const input1=document.getElementById("number1")! as HTMLInputElement;
+const input2=document.getElementById("number2")! as HTMLInputElement;
+
+function add(num1:number,num2:number){
+    return num1+num2;
+}
+
+button.addEventListener("click",function(){
+    console.log(add(+input1.value,+input2.value));
+});
